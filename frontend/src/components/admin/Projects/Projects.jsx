e3b169fa872ff46e8bb0b2f6fd23f01dd862e2d4
samiullah51 <samii51728@gmail.com>
2023-08-22 14:37:33 +0500

    add project form # add service form done

--- frontend/src/components/admin/Projects/Projects.jsx
+++ frontend/src/components/admin/Projects/Projects.jsx
@@ -1,18 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Services.css";
 import SingleProject from "./SingleProject";
+import AddService from "../Services/AddService";
+import AddProject from "./AddProjects";
 function Projects() {
+  const [modal, setModal] = useState(false);
+
   return (
     <div className="services">
       <div className="services__header">
         <p>Projects</p>
-        <a href="">Add New</a>
+        <div className="add__new" onClick={() => setModal(!modal)}>
+          Add New
+        </div>
       </div>
       <SingleProject />
       <SingleProject />
       <SingleProject />
       <SingleProject />
       <SingleProject />
+
+      <div
+        className="servce__modal"
+        style={{ transform: modal ? "translateY(0%)" : "translateY(-100%)" }}
+      >
+        <AddProject setModal={setModal} />
+        <div className="modal__close" onClick={() => setModal(false)}>
+          X
+        </div>
+      </div>
     </div>
   );
 }
