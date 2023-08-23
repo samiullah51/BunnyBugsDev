acba5c64a692b02ddb355c1564559bebfd8a1053
AliZahid959 <alizahid550@gmail.com>
2023-08-23 20:23:06 +0500

    changes in header blade

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -24,7 +24,7 @@ function Header({ route }) {
                     <a href="#">Linkedin</a>
                   </li>
                   <li className="social-item">
-                    <a href="#">Instag   ram</a>
+                    <a href="#">Instagram</a>
                   </li>
                 </ul>
               </div>
