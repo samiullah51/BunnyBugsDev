7dc525b778d85fe95bd05cc49672e603c9ecca2a
samiullah51 <samii51728@gmail.com>
2023-08-28 16:43:35 +0500

    check for deployment

--- custom_frontend/vite.config.js
+++ custom_frontend/vite.config.js
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  base: "http://127.0.0.1:5173", // Change this to your actual base URL
+});
