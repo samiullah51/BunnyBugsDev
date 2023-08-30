8791472c359e04a7c9be0f493cb311a776c6457c
AliZahid959 <alizahid550@gmail.com>
2023-08-30 19:07:18 +0500

    changes in home page

--- custom_frontend/src/components/Featured/SingleFeaturedCard.jsx
+++ custom_frontend/src/components/Featured/SingleFeaturedCard.jsx
@@ -3,7 +3,7 @@ import "./Featured.css";
 function SingleFeaturedCard({ icon, title, desc }) {
   return (
     <div className="single__featured">
-      <img src={icon} alt="" />
+      <span className="mt-2 me-4 icon-bg p-3" style={{color:'var(--bs-white)'}}>{icon}</span>
       <div>
         <p className="title">{title}</p>
         <p className="desc__f">{desc}</p>
