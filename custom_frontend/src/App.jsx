fbdb42574c6ae1d098cf3f61b547b0b0790afccc
samiullah51 <samii51728@gmail.com>
2023-08-31 12:38:40 +0500

    cleaned up

--- custom_frontend/src/App.jsx
+++ custom_frontend/src/App.jsx
@@ -1,12 +1,7 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import HomePage from "./pages/HomePage";
 import AboutPage from "./pages/AboutPage";
 import QuotationForm from "./pages/QuotationForm";
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
       <HomePage />
