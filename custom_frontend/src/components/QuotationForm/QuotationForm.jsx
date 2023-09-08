980e157aaa11df5c7396f3c69c04a7449898909f
samiullah51 <samii51728@gmail.com>
2023-09-08 17:46:12 +0500

    working on quotation

--- custom_frontend/src/components/QuotationForm/QuotationForm.jsx
+++ custom_frontend/src/components/QuotationForm/QuotationForm.jsx
@@ -7,14 +7,21 @@ import TabletIcon from "@mui/icons-material/Tablet";
 import CheckCircleIcon from "@mui/icons-material/CheckCircle";
 function QuotationFormComp() {
   const [price, setPrice] = useState(0);
-  const [data, setData] = useState([
+  const [data, setData] = useState([]);
+  const [selected, setSelected] = useState(false);
+  const [types, setTypes] = useState([
+    { id: 1, type: "Web App" },
+    { id: 2, type: "Mobile App" },
+    { id: 3, type: "Desktop App" },
+  ]);
+  const webData = [
     {
       id: 1,
       qst: "What do you want",
       options: {
-        a: { cat: "Mobile App", price: 100 },
-        b: { cat: "Web App", price: 200 },
-        c: { cat: "Desktop App", price: 300 },
+        a: { cat: "Landing Page", price: 100 },
+        b: { cat: "Business Website", price: 200 },
+        c: { cat: "Wordpress", price: 300 },
       },
     },
     {
@@ -45,51 +52,91 @@ function QuotationFormComp() {
         d: { cat: "do not know", price: 0 },
       },
     },
-  ]);
+  ];
+  const appData = [
+    {
+      id: 1,
+      qst: "Which App",
+      options: {
+        a: { cat: "Ecommerce App", price: 100 },
+        b: { cat: "Android App", price: 200 },
+        c: { cat: "IOS App", price: 300 },
+      },
+    },
+    {
+      id: 2,
+      qst: "Do you need a special functionality?",
+      options: {
+        a: { cat: "Yes", price: 100 },
+        b: { cat: "No", price: 200 },
+        c: { cat: "Do not Know", price: 0 },
+      },
+    },
+    {
+      id: 3,
+      qst: "How will you make money from your website?",
+      options: {
+        a: { cat: "Adds", price: 120 },
+        b: { cat: "Blogging", price: 240 },
+        c: { cat: "No Money", price: 0 },
+      },
+    },
+    {
+      id: 4,
+      qst: "Do you need website strategy to get more revenue and profit?",
+      options: {
+        a: { cat: "Addss", price: 120 },
+        b: { cat: "Blogging", price: 240 },
+        c: { cat: "No Money", price: 0 },
+        d: { cat: "do not know", price: 0 },
+      },
+    },
+  ];
   const [index, setIndex] = useState(0);
-  const handleClick = (p) => {
-    setPrice(price + p);
+  const handleClick = (type) => {
+    setSelected(true);
+    // https://localhost:8000/api/v1/bunnybugsdev/apps?type=Web%App
+    // if (type === "Web App") {
+    //   setData(webData);
+    // } else {
+    //   setData(appData);
+    // }
+  };
+  const handleIndex = () => {
     setIndex(index + 1);
   };
-
-  return index !== data.length ? (
+  return (
     <div className="quotation">
       <span className="price">US${price}</span>
       <p className="ready">READY TO GET STARTED?</p>
-      <h2>{data[index].qst}</h2>
-      <div className="options">
-        <p
-          onClick={() => handleClick(data[index].options.a.price)}
-          className="simple"
-        >
-          <CheckCircleIcon />
-          <span> {data[index].options.a.cat}</span>
-        </p>
-        <p
-          onClick={() => handleClick(data[index].options.b.price)}
-          className="simple"
-        >
-          <MobileFriendlyIcon />
-          <span> {data[index].options.b.cat}</span>
-        </p>
-        <p
-          onClick={() => handleClick(data[index].options.c.price)}
-          className="simple"
-        >
-          <TabletIcon />
-          <span> {data[index].options.c.cat}</span>
-        </p>
-      </div>
-    </div>
-  ) : (
-    <div className="quotation">
-      <p className="ready">YOUR WEBSITE ESTIMATE</p>
-      <h1>US${price}</h1>
-      <p className="get__in__touch">
-        Enter your email address below and we’ll be in touch:
-      </p>
-      <input type="text" placeholder="Email address " />
-      <a href="">LET'S GO!</a>
+      {data.length === 0 ? (
+        <h2>Select Your Choice</h2>
+      ) : (
+        <h2>{data[index].qst}</h2>
+      )}
+      {!selected && (
+        <div className="options">
+          {types.map((single) => (
+            <div className="simple" onClick={() => handleClick(single.type)}>
+              <MobileFriendlyIcon />
+              <p>{single.type}</p>
+            </div>
+          ))}
+        </div>
+      )}
+      {data.length > 0 && (
+        <div className="options">
+          <div className="simple" onClick={handleIndex}>
+            {data[index].options.a.cat}
+          </div>
+          <div className="simple" onClick={handleIndex}>
+            {data[index].options.b.cat}
+          </div>
+          <div className="simple" onClick={handleIndex}>
+            {data[index].options.c.cat}
+          </div>
+        </div>
+      )}
     </div>
   );
 }
